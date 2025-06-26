82f521eb996e7fb89af193c88e35d19d076ac238
dajiebi <535663007@qq.com>
2025-06-26 10:01:39 +0800

    修改GIF部分

--- src/components/sections/Carousel.vue
+++ src/components/sections/Carousel.vue
@@ -25,6 +25,9 @@ export default {
         "./carousel/3.gif",
         "./carousel/4.gif",
         "./carousel/5.gif",
+        "./carousel/8.gif",
+        "./carousel/7.gif",
+        "./carousel/6.gif",
       ],
     }
   }
